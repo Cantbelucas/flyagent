9d99aece18758e00a610aa8c82851f702a5872ec
Cantbelucas <lucasmaj08@gmail.com>
2026-09-01 21:48:01 +0200

    Delete flyagent directory

--- flyagent/FJERN-AUTOMATIK.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-title Fjern automatik
-echo.
-schtasks /Delete /TN "Flyagent - daglig soegning" /F
-echo.
-echo   Den daglige soegning er slaaet fra.
-echo.
-pause
--- flyagent/KOER-STILLE.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-set PYTHONIOENCODING=utf-8
-if not exist "resultater" mkdir "resultater"
-python soeg.py --stille >> "resultater\log.txt" 2>&1
